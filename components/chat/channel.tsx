bb0727a210f06a6af01e8116c4cd6fcf04453208
Themba <mndebele.themba@gmail.com>
2025-03-08 19:01:20 +0200

    Add QR code functionality, update Ably API key handling, and increment site version

--- components/chat/channel.tsx
+++ components/chat/channel.tsx
@@ -6,9 +6,10 @@ import Chat from "./chat";
 import Channels from "./channels";
 import { Button } from "../ui/button";
 import { Badge } from "../ui/badge";
-import { RssIcon } from "@/config/icons";
+import { QrCodeIcon, RssIcon } from "@/config/icons";
 import { ProfileMenu } from "./profile-menu";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import QrDialog from "./qr-dialog";
 
 function Channel() {
   const [channel, setChannel] = useState("first");
@@ -17,7 +18,12 @@ function Channel() {
   return (
     <ChannelProvider channelName={channel}>
       <div className="flex flex-col h-full w-full">
-        <div className="flex justify-center items-center relative z-10 p-4">
+        <div className="flex justify-between items-center relative z-10 p-4">
+          <QrDialog channel={channel}>
+            <Button size="icon" variant="ghost">
+              <QrCodeIcon className="!w-6 !h-6 text-icon" />
+            </Button>
+          </QrDialog>
           <Channels
             channel={channel}
             channels={channels}
@@ -38,21 +44,20 @@ function Channel() {
               </Badge>
             </Button>
           </Channels>
-          <div className="absolute right-4 z-10">
-            <ProfileMenu>
-              <div className="h-9 w-9 relative">
-                <Avatar className="h-9 w-9">
-                  <AvatarImage
-                    src={`https://www.tapback.co/api/avatar/user55?color=3`}
-                    alt={"Themba Mndebele"}
-                  />
-                  <AvatarFallback className="text-xs uppercase">
-                    TP
-                  </AvatarFallback>
-                </Avatar>
-              </div>
-            </ProfileMenu>
-          </div>
+
+          <ProfileMenu>
+            <div className="h-9 w-9 relative">
+              <Avatar className="h-9 w-9">
+                <AvatarImage
+                  src={`https://www.tapback.co/api/avatar/user55?color=3`}
+                  alt={"Themba Mndebele"}
+                />
+                <AvatarFallback className="text-xs uppercase">
+                  TP
+                </AvatarFallback>
+              </Avatar>
+            </div>
+          </ProfileMenu>
         </div>
         <div className="grow w-full relative">
           <Chat channelName={channel} />

2e7f91b46f18f2c1970e479a1aade4346f7c1324
Themba <mndebele.themba@gmail.com>
2025-03-11 22:37:28 +0200

    Wrap CreateChannelCard in Suspense for improved loading state handling

--- app/channels/create/page.tsx
+++ app/channels/create/page.tsx
@@ -2,19 +2,21 @@
 import { useSearchParams } from "next/navigation";
 
 import CreateChannelCard from "@/components/channels/create/card";
+import { Suspense } from "react";
 
 function CreateChannelPage() {
   let channelName = "";
   const params = useSearchParams();
   if (params.has("channelName")) {
     channelName = params.get("channelName")!;
-  } else {
   }
 
   return (
-    <div className="p-6 h-full w-full space-y-6">
-      <CreateChannelCard channelName={channelName} />
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="p-6 h-full w-full space-y-6">
+        <CreateChannelCard channelName={channelName} />
+      </div>
+    </Suspense>
   );
 }
 
